00f5f949739c6f92c54b27e7a1b841212f845308
hemant052004 <kashyaphemant2004@gmail.com>
2024-08-29 08:31:37 +0530

    fix

--- begam/src/api/axios.js
+++ begam/src/api/axios.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://daliy-matchs-backend.onrender.com/api/user',
+    baseURL: 'https://daliy-matchs-backend-rp07.onrender.com/api/user',
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
 const authAxiosInstance = axios.create({
-    baseURL: 'https://daliy-matchs-backend.onrender.com/api/user',
+    baseURL: 'https://daliy-matchs-backend-rp07.onrender.com/api/user',
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
 const authAdminInstance = axios.create({
-    baseURL: 'https://daliy-matchs-backend.onrender.com/api/admin',
+    baseURL: 'https://daliy-matchs-backend-rp07.onrender.com/api/admin',
     headers: {
         'Content-Type': 'application/json',
     },
